4c7d47fc79cff1d30aca6cac6e38b493d44135ae
Nasser Amini <namini40@gmail.com>
2022-05-29 22:12:14 +0430

    first

---
+++ android/app/src/main/java/amini/codebaz/codebaz_musical/MainActivity.java
@@ -0,0 +1,6 @@
+package amini.codebaz.codebaz_musical;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(CodeBazMusical());
+
+class CodeBazMusical extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
